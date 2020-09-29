5f4549aa3d443a0182b157b872a0ed58b6edb8ea
Dimitris Lappas <gimys91@hotmail.com>
2020-09-29 21:40:37 +0300

    Added more channels to filter

--- Filtering.lua
+++ Filtering.lua
@@ -3,6 +3,43 @@ ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", function(self,event,msg,auth
     return MyChatFilter(self,event,msg,author)
 end)
 
+ChatFrame_AddMessageEventFilter("CHAT_MSG_GUILD", function(self,event,msg,author)
+    return MyChatFilter(self,event,msg,author)
+end)
+
+ChatFrame_AddMessageEventFilter("CHAT_MSG_OFFICER", function(self,event,msg,author)
+    return MyChatFilter(self,event,msg,author)
+end)
+
+ChatFrame_AddMessageEventFilter("CHAT_MSG_PARTY", function(self,event,msg,author)
+    return MyChatFilter(self,event,msg,author)
+end)
+
+ChatFrame_AddMessageEventFilter("CHAT_MSG_PARTY_LEADER", function(self,event,msg,author)
+    return MyChatFilter(self,event,msg,author)
+end)
+
+ChatFrame_AddMessageEventFilter("CHAT_MSG_RAID", function(self,event,msg,author)
+    return MyChatFilter(self,event,msg,author)
+end)
+
+ChatFrame_AddMessageEventFilter("CHAT_MSG_RAID_LEADER", function(self,event,msg,author)
+    return MyChatFilter(self,event,msg,author)
+end)
+
+ChatFrame_AddMessageEventFilter("CHAT_MSG_SAY", function(self,event,msg,author)
+    return MyChatFilter(self,event,msg,author)
+end)
+
+ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER", function(self,event,msg,author)
+    return MyChatFilter(self,event,msg,author)
+end)
+
+ChatFrame_AddMessageEventFilter("CHAT_MSG_YELL", function(self,event,msg,author)
+    return MyChatFilter(self,event,msg,author)
+end)
+
+
 function MyChatFilter(self,event,msg,author)
     for index, value in ipairs(AntiSpam_Database.global.bannedWords) do
         if string.find(msg:lower(), value:lower()) then

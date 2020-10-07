cd911a5fedce03c11938ca5ba8b0a7b696d48317
Dimitris Lappas <gimys91@hotmail.com>
2020-10-07 21:44:03 +0300

    version 1.2 Grids added

--- Main.lua
+++ Main.lua
@@ -1,5 +1,6 @@
 AntiSpam = LibStub("AceAddon-3.0"):NewAddon("AntiSpam", "AceConsole-3.0", "AceEvent-3.0")
 
+
 local defaults = {
   global = {
       bannedWords = {},
@@ -7,19 +8,26 @@ local defaults = {
   locale = {
     ingoredPlayers = {}
   }
-}
+};
 
-IsUiOpened = false
+IsUiOpened = false;
+
+GridInitialized = false;
+GridDataIgnoreByKeyWord = {};
+GridDataIgnoreByName = {};
+GridComponent = {}
 
 function AntiSpam:OnInitialize()
-  AntiSpam_Database = LibStub("AceDB-3.0"):New("AntiSpamDB",defaults, true)
-  AntiSpam:RegisterChatCommand("antispam", "AntiSpamCommand")
+  AntiSpam_Database = LibStub("AceDB-3.0"):New("AntiSpamDB",defaults, true);
+  AntiSpam:RegisterChatCommand("antispam", "AntiSpamCommand");
+  AntiSpam:Print("Hello and thanks for using AntiSpam addon. If you have any suggestion don't hesitate to write it in the comments")
+  AntiSpam:Print("for UI type /antispam")
 end
 
 function AntiSpam:AntiSpamCommand(input)
   if not IsUiOpened then
-    AntiSpam_Gui:ShowGui()
-    IsUiOpened = true
+    AntiSpam_Gui:ShowGui();
+    IsUiOpened = true;
   end
 end
 
